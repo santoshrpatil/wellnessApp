d7c323284070bbcd3f16e53dea60a4a89a3013c1
sntsptl <patil.santosh25@gmail.com>
2021-01-12 08:53:26 +0530

    first commit

---
+++ src/main/java/com/bootcamp/wellness/WellnessApp/WellnessAppApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.wellness.WellnessApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/wellness/WellnessApp/WellnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.wellness.WellnessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
